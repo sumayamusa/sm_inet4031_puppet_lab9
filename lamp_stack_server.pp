f2ab9b81e0d4b0baf87abaf3b03e9273784de815
Sumaya Musa <sumayamusa142@gmail.com>
2025-11-19 23:17:34 +0000

    Add Puppet manifests

---
+++ lamp_stack_server.pp
@@ -0,0 +1,50 @@
+package { 'apache2':
+  ensure => installed,
+}
+
+package { 'php':
+  ensure => installed,
+  notify  => Service['apache2'],
+  require => [Package['apache2']],
+}
+
+file { '/var/www/html/phpinfo.php':
+  ensure  => file,
+  source  => '/home/sumayamusa/sm_inet4031_puppet_lab9/phpinfo.php',
+  notify  => Service['apache2'],
+  require => Package['apache2'],
+}
+
+service { 'apache2':
+  ensure => running,
+  enable => true,
+  require => [Package['apache2'], Package['php']]
+}
+
+package { 'libapache2-mod-php':
+  ensure  => installed,
+  notify  => Service['apache2'],
+  require => Package['php'],
+}
+
+package { 'php-cli':
+  ensure  => installed,
+  notify  => Service['apache2'],
+  require => Package['php'],
+}
+
+package { 'php-mysql':
+  ensure  => installed,
+  notify  => Service['apache2'],
+  require => Package['php'],
+}
+
+package { 'mariadb-server':
+  ensure => installed,
+}
+
+service { 'mariadb':
+  ensure  => running,
+  enable  => true,
+  require => Package['mariadb-server'],
+}
